ad5e2bc19a1b223efa0d821289627ab894f1471b
johntr <jtredlich@gmail.com>
2016-06-08 23:46:15 -0400

    Added color matching based on belt/shoes. Setup rules system to get granular with our outfits

--- randomoutfit/randomoutfit.js
+++ randomoutfit/randomoutfit.js
@@ -11,61 +11,147 @@ Module.register("randomoutfit", {
     //default
     defaults: {
         clothes: {
-            shirts: [
-                'Burgundy Polo',
-                'Navy Polo',
-                'Blue and Salmon Striped Polo',
-                'Flannel Button Down'
-            ],
-            pants: [
-                'Dark Khakis',
-                'light Khakis',
-                'Jeans'
-            ],
-            socks: [
-                'Red Striped',
-                'Blue',
-                'Grey Stripes',
-                'Black'
-            ]
+            black : {
+                shirts: [
+                    'Burgundy Polo',
+                    'Navy Polo',
+                    'Blue and Salmon Striped Polo',
+                ],
+                pants: [
+                    'Dark Khakis',
+                    'light Khakis',
+                    'grey pants'
+                ],
+                socks: [
+                    'Red Striped',
+                    'Grey Stripes',
+                    'Black'
+                ]
+            },
+            brown: {
+                shirts: [
+                    'Burgundy Polo',
+                    'Navy Polo',
+                    'Blue and Salmon Striped Polo'
+                ],
+                pants: [
+                    'Dark Khakis',
+                    'light Khakis'
+                ],
+                socks: [
+                    'Red Striped',
+                    'Blue'
+                ]
+            },
+            casual: {
+                shirts: [
+                    'NASA T-Shirt',
+                    'Thor T-Shirt',
+                    'Iron Man T-Shirt',
+                    'Capt America T-Shirt',
+                    'Flannel button down'
+                ],
+                pants: [
+                    'Dark jeans',
+                    'Light jeans'
+                ],
+                socks: [
+                    'Black no-show',
+                    'White no-show'
+                ]
+            }
         },
         updateInterval: 1000
     },
-
+    //maybe if we pick a random color scheme first we can organize data that way. 
     // Define required scripts.
-    // getScripts: function() {
-    //     return ["moment.js"];
-    // },
+    getScripts: function() {
+        return ["moment.js"];
+    },
 
    start: function() {
        Log.info('Starting module: ' + this.name);
-
+       moment.locale(config.language);
    },
-    getShirts: function() {
-        return this.defaults.clothes.shirts;
+    getShirts: function(color) {
+        return this.defaults.clothes[color].shirts;
     },
-    getPanits: function() {
-        return this.defaults.clothes.pants;
+    getPants: function(color) {
+        return this.defaults.clothes[color].pants;
     },
-    getSocks: function() {
-        return this.defaults.clothes.socks;
+    getSocks: function(color) {
+        return this.defaults.clothes[color].socks;
     },
 
-    randomNumber: function () {
-        return Math.floor((Math.random()*4));
+    color: "",
+
+    rules: [
+        //check to see if its a casual day.
+        function (self) {
+            var day = moment().format('e');
+
+            if(self.color == "") {
+                switch (day) {
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                        self.color = "";
+                        break;
+                    case 5:
+                    case 6:
+                    case 0:
+                        self.color = "casual";
+                        break;
+                }
+            }
+            return self.color;
+        },
+        //if its not a casual day get a dress up color.
+        function (self) {
+
+            var numDay = self.randomNumber(2);
+            if(self.color == "") {
+                switch (numDay) {
+                    case 0:
+                        self.color = "black";
+                        break;
+                    case 1:
+                        self.color = "brown";
+                        break;
+                    default:
+                        self.color = "";
+                }
+            }
+            return self.color;
+        }
+        //@TODO rule based on weather(add shorts or sweaters). Can we get the weather from the default weather modules? or Duplicate code?
+    ],
+
+    randomNumber: function (count) {
+        return Math.floor((Math.random()*count));
+    },
+    getColor: function() {
+        var color;
+        var self = this;
+        for(var i = 0; i< this.rules.length; i++) {
+            color = this.rules[i](self);
+        }
+        return color;
     },
 
     getDom: function() {
-            var shirt = this.getShirts();
-            var pants = this.getPanits();
-            var socks = this.getSocks();
+        var c = this.getColor();
+        var shirt = this.getShirts(c);
+        var pants = this.getPants(c);
+        var socks = this.getSocks(c);
 
-            var text = "Today you should wear, a " + shirt[this.randomNumber()] + " with " + pants[this.randomNumber()] + " and rock those " + socks[this.randomNumber()] + " socks!";
-        //var shirts = this.randomNumber();
+        var displayText = "Today you should wear, a " + shirt[this.randomNumber(shirt.length)] + " with " + pants[this.randomNumber(pants.length)] + " and rock those " + socks[this.randomNumber(socks.length)] + " socks!";
+       // var displayText = c;
         //var testText = document.createTextNode(test);
         var wrapper = document.createElement("div");
         wrapper.className = "thin medium bright";
-        wrapper.innerHTML = text;
+        wrapper.innerHTML = displayText;
 
         return wrapper;
     }
